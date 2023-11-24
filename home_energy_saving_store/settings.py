97782eb47d4ebe51816133357ca003f3f7bab190
Pimmz <>
2023-11-24 12:15:51 +0000

    Update setting for deployment again

--- home_energy_saving_store/settings.py
+++ home_energy_saving_store/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['home-energy-saving-store-be6e9d82e03c.herokuapp.com', 'localhost', '8000-pimmz-home-energy-saving-vfkzjjtzgc.us2.codeanyapp.com', '8001-pimmz-home-energy-saving-vfkzjjtzgc.us2.codeanyapp.com']
 
