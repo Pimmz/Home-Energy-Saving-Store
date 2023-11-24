a5ed3a7f8cb95ac67dfb7c4709ce98a288671a23
Pimmz <>
2023-11-24 11:46:09 +0000

    Update setting for deployment

--- home_energy_saving_store/settings.py
+++ home_energy_saving_store/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['home-energy-saving-store.herokuapp.com', 'localhost', '8000-pimmz-home-energy-saving-vfkzjjtzgc.us2.codeanyapp.com', '8001-pimmz-home-energy-saving-vfkzjjtzgc.us2.codeanyapp.com']
+ALLOWED_HOSTS = ['home-energy-saving-store', 'localhost', '8000-pimmz-home-energy-saving-vfkzjjtzgc.us2.codeanyapp.com', '8001-pimmz-home-energy-saving-vfkzjjtzgc.us2.codeanyapp.com']
 
 
 # Application definition
