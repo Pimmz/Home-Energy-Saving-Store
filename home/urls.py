3ff96f9f378e80539b45bfa5e85b80f70fecc404
Pimmz <>
2023-12-12 11:29:58 +0000

    Add custom error pages

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
+from .views import BadRequestView, PermissionDeniedView, PageNotFoundView, ServerErrorView
 from . import views
 
 
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', views.index, name='home'),
+    path('400/', BadRequestView.as_view(), name='bad_request'),
+    path('403/', PermissionDeniedView.as_view(), name='permission_denied'),
+    path('404/', PageNotFoundView.as_view(), name='page_not_found'),
+    path('500/', ServerErrorView.as_view(), name='server_error'),
+    
 ]
--- home/views.py
+++ home/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views import View
 
 # Create your views here.
 
@@ -7,3 +8,24 @@ def index(request):
     """ A view to return the index page """
 
     return render(request, 'home/index.html')
+
+
+class BadRequestView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'home/400.html', status=400)
+
+
+class PermissionDeniedView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'home/403.html', status=403)
+
+
+class PageNotFoundView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'home/404.html', status=404)
+
+
+class ServerErrorView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, 'home/500.html', status=500)
+
